203bd691492f2402fc278a0e0bfb4b0024fa33ec
Darkin <darkinb255@gmail.com>
2020-02-23 20:37:05 +0200

    fake movies db

---
+++ services/movieservices.js
@@ -0,0 +1,38 @@
+
+const movies = [
+    {
+        _id : 1,
+        title : "The Irishman",
+        genre : {_id : 1 , name : "Crime"},
+        publishYear : "2019",
+        numberInStock : 4
+} ,
+{
+    _id : 2,
+    title : "The Lighthouse",
+    genre : {_id : 2 , name : "Drama"},
+    publishYear : "2019",
+    numberInStock : 1
+} ,
+{
+    _id : 3,
+    title : "1917",
+    genre : {_id : 3 , name : "Drama"},
+    publishYear : "2019",
+    numberInStock : 6
+}  , 
+{
+    _id : 4,
+    title : "Once Upon a Time in Hollywood",
+    genre : {_id : 4 , name : "Comedy-drama"},
+    publishYear : "2019",
+    numberInStock : 4
+} ,
+{
+    _id : 5,
+    title : "Joker",
+    genre : {_id : 5 , name : "Drama"},
+    publishYear : "2019",
+    numberInStock : 7
+} 
+]
