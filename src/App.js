b21853a7abdbec88a690069e1822b2e3f5af8c07
Darkin <darkinb255@gmail.com>
2020-02-23 22:31:49 +0200

    fetch from firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import database from './config/firebaseConfig';
+const movielist =  database.collection("movies-list");
 
-function App() {
+
+function App() { 
+  movielist.get().then((d)=>{
+    d.docs.forEach(doc=>{
+      console.log(doc.data());
+    });
+  })
   return (
     <main className = "container" >
-      
     </main>
   );
 }
-
 export default App;
