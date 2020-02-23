b3fc9b3b7eb584866068efad07008e0280efbe02
Darkin <darkinb255@gmail.com>
2020-02-23 22:58:30 +0200

    Find movie by id

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
-import {getMovies} from './utility/movies.js';
+import {getMovies,findMovie} from './utility/movies.js';
 
 function App() { 
-
+findMovie(4).then(d=>{
+  console.log(d);
+})
   return (
     <main className = "container" >
     </main>
--- src/utility/movies.js
+++ src/utility/movies.js
@@ -2,6 +2,11 @@ import database from "../config/firebaseConfig";
 
 export async function getMovies(){
     console.log("yo");
-      let  movies = await database.collection('movies-list').get();
+      let  movies = await database.collection('movies').get();
       return movies.docs.map(doc=>doc.data());
+}
+
+export async function findMovie(id){
+      let  movies = await database.collection('movies').where('_id','==',id).get();
+      return movies.docs.map(doc=>doc.data())[0];
 }
