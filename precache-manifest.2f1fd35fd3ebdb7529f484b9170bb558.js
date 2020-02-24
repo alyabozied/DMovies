7091f5c312b07acd022102205ca6c11bc56fd527
Darkin <darkinb255@gmail.com>
2020-02-24 17:26:20 +0200

    Updates

---
+++ precache-manifest.2f1fd35fd3ebdb7529f484b9170bb558.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e71e1823fd0271ee6249f917677472",
+    "url": "/DMovies/index.html"
+  },
+  {
+    "revision": "6d6a39a1e6fbee0f5216",
+    "url": "/DMovies/static/css/2.b81190d2.chunk.css"
+  },
+  {
+    "revision": "15c650dcfa067e6b9e39",
+    "url": "/DMovies/static/css/main.80663e25.chunk.css"
+  },
+  {
+    "revision": "6d6a39a1e6fbee0f5216",
+    "url": "/DMovies/static/js/2.f0acd8e3.chunk.js"
+  },
+  {
+    "revision": "7f84d11092c10d3a43893d78f68c23cb",
+    "url": "/DMovies/static/js/2.f0acd8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c650dcfa067e6b9e39",
+    "url": "/DMovies/static/js/main.8ece3f9d.chunk.js"
+  },
+  {
+    "revision": "70347d5f2fd9ec45687d",
+    "url": "/DMovies/static/js/runtime-main.4f74162f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/DMovies/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/DMovies/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/DMovies/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/DMovies/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/DMovies/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
