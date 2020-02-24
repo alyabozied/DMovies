e0c33617861cbaa4d68c3209bb833b1c8b824569
Darkin <darkinb255@gmail.com>
2020-02-24 22:00:38 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="head text-center bg-primary">
+           <div className=" text-white display-1 py-auto "> D-MOVIES </div>
+           <p className=" subtext badge badge-sm badge-light text-primary">BETA</p>
+        </div>
        <MoviesList/>
       </div>
     );
