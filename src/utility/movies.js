153f3337e7cb1b9f366807cce97751ce8d76900e
Darkin <darkinb255@gmail.com>
2020-02-24 15:07:13 +0200

    fetch doc id

--- src/utility/movies.js
+++ src/utility/movies.js
@@ -2,11 +2,12 @@ import database from "../config/firebaseConfig";
 
 export async function getMovies(){
     console.log("yo");
-      let  movies = await database.collection('movies').get();
-      return movies.docs.map(doc=>doc.data());
+      let  movies = await database.collection('movies-list').get();
+      return movies.docs.map(doc=>({ ...doc.data(),ID:doc.id }));
+    
 }
 
 export async function findMovie(id){
-      let  movies = await database.collection('movies').where('_id','==',id).get();
+      let  movies = await database.collection('movies-list').where('_id','==',id).get();
       return movies.docs.map(doc=>doc.data())[0];
 }
